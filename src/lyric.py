5619f09bacb998b15ce2fe5863713d9f73dfeb7f
Bugsounet - Cédric <github@bugsounet.fr>
2022-09-19 01:54:31 +0200

    prevent crash

--- src/lyric.py
+++ src/lyric.py
@@ -1,6 +1,7 @@
 import requests
 
 def get_lyrics_for_track(access_token, track_id):
+    lyrics = []
     try:
         resp = requests.get(
             "https://spclient.wg.spotify.com/lyrics/v1/track/"+track_id,
@@ -12,8 +13,10 @@ def get_lyrics_for_track(access_token, track_id):
     except:
         raise ConnectionError
 
-    response = resp.json()
-
-    if (response.get("lines")):
-       return response.get("lines")
-    return 0
+    if (resp.text and resp.json()):
+      response = resp.json()
+      lyrics = response.get("lines")
+    else:
+     raise AttributeError   
+ 
+    return lyrics
